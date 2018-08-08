88d054806fa79df8a14976d77e78d022c339e561
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-08 15:18:15 -0700

    Set up audio update and removing old audios

--- imports/ui/components/uploadForm.js
+++ imports/ui/components/uploadForm.js
@@ -15,20 +15,18 @@ class UploadForm extends Component {
       Template.sounds,
       ReactDOM.findDOMNode(this.refs.sounds)
     );
-
   }
   cleanForm() {
     Blaze.remove(this.sounds); // Clean up Blaze view
- 
   }
   componentWillUnmount() {
     this.cleanForm();
   }
   render() {
     return (
-        <div>
-          <span ref="sounds" />
-        </div>
+      <div>
+        <span ref="sounds" />
+      </div>
     ); // Render a placeholder
   }
 }
@@ -50,14 +48,12 @@ Template.sounds.helpers({
 
 Template.files.helpers({
   audioFile() {
-    console.log("Andreisadfasdfasd", Audio.collection._collection._docs)
+    console.log("Andreisadfasdfasd", Audio.collection._collection._docs);
     return Audio.findOne({ userId: Meteor.userId() });
-    
   },
-  allAudioFiles(){
+  allAudioFiles() {
     return Audio.find();
   },
-
 
   user() {
     return Meteor.userId();
@@ -67,35 +63,40 @@ Template.files.helpers({
 Template.uploadForm.events({
   "change #fileInput"(e, template) {
     if (e.currentTarget.files && e.currentTarget.files[0]) {
-      // We upload only one file, in case
-      // multiple files were selected
-      console.log("Uploading....");
-      console.log(e.currentTarget.files[0]);
-      const upload = Audio.insert(
-        {
-          file: e.currentTarget.files[0],
-          streams: "dynamic",
-          chunkSize: "dynamic"
-        },
-        false
-      );
+      !Audio.findOne({ userId: Meteor.userId() })
+        ? (upload = Audio.insert(
+            {
+              file: e.currentTarget.files[0],
+              streams: "dynamic",
+              chunkSize: "dynamic"
+            },
+            false
+          ))
+        : Audio.remove(
+            { _id: Audio.findOne({ userId: Meteor.userId() }, { _id: 1 })._id },
+            () => {
+              return (
+                console.log("file removed"),
+                (upload = Audio.insert(
+                  {
+                    file: e.currentTarget.files[0],
+                    streams: "dynamic",
+                    chunkSize: "dynamic"
+                  },
 
-      // upload.on('start', function () {
-      //   template.currentUpload.set(this);
-      // });
+                  console.log("UPLOADED?")
+                ))
+              );
+            }
+          );
 
-      upload.on("end", function(error, fileObj) {
-        console.log("File Object",fileObj)
-        if (error) {
-          alert("Error during upload: " + error);
-        } else {
-          alert('File "' + fileObj.name + '" successfully uploaded');
-        }
-        //   template.currentUpload.set(false);
+      upload.on("end", function(fileObj) {
+        console.log("File Object", fileObj);
+
+        alert('File "' + fileObj.name + '" successfully uploaded');
       });
 
       upload.start();
-      
     }
   }
 });
