6f745d9b3a869788b97e387623753360200b8955
im-chang <mchang0718@gmail.com>
2018-08-08 17:08:00 -0700

    add SinglesQueueCard into profile card

--- imports/ui/components/ProfileCard/ProfileCard.js
+++ imports/ui/components/ProfileCard/ProfileCard.js
@@ -1,47 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import SinglesQueueCard from "../../components/SinglesQueueCard/SinglesQueueCard";
-import { Singles } from '../../../api/singles';
-import { Tracker } from 'meteor/tracker'
-import CardHeader from '@material-ui/core/CardHeader'
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Singles } from "../../../api/singles";
+import { Tracker } from "meteor/tracker";
+import CardHeader from "@material-ui/core/CardHeader";
+import SinglesQueueCard from "../SinglesQueueCard/SinglesQueueCard";
 
 const styles = {
   card: {
-    maxWidth: 345,
+    maxWidth: 345
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
+    paddingTop: "56.25%" // 16:9
+  }
 };
 
-const ProfileCard= ({classes,name,bio}) => {
-    return(
-        <Card className={classes.card}>
-        <CardContent>
+const ProfileCard = ({ classes, name, bio }) => {
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <SinglesQueueCard />
+        <Typography gutterBottom variant="headline" component="h2">
+          {name}
+        </Typography>
         <Typography
-            gutterBottom variant="headline" component="h2">
-            {name}
-          </Typography>
-          <Typography gutterBottom variant="sub-heading" style={{color:'black'}}>
-            {bio}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="outlined" color="secondary">
-            Right!
-          </Button>
-        </CardActions>
-      </Card>
-    )
-}
+          gutterBottom
+          variant="sub-heading"
+          style={{ color: "black" }}
+        >
+          {bio}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="outlined" color="secondary">
+          Right!
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default withStyles(styles)(ProfileCard);
